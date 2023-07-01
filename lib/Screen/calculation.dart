b277914a72d42ab20672702846eee7436c1a2700
Tricha Abdelmoughit <katsu.sacri@gmail.com>
2023-07-01 22:15:49 +0100

    Pass data to another screen

--- lib/Screen/calculation.dart
+++ lib/Screen/calculation.dart
@@ -1,14 +1,40 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class CalculationScreen extends StatelessWidget {
-  const CalculationScreen({super.key});
+  late int _number ;
+  late String _operation;
+  late String _message;
+
+  CalculationScreen(int number, String operation) {
+    int? result ;
+
+
+    _number = number;
+    _operation = operation;
+
+    if (_operation == "opposite")
+      result = -_number;
+    else if (_operation == "absolute_value")
+      result = _number.abs();
+    else if(_operation == "square")
+      result = pow(_number, 2) as int?;
+    else {
+      _message = "Invalid Operation";
+      return;
+    }
+
+    _message = "The $_operation of $_number is $result";
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return TextButton(onPressed : () {
       Navigator.of(context).pop(); // TO RETURN BACK
     },
-        child: Text("Go Back !!"),
+        child: Text(_message),
     );
   }
 }
